e9b54a7a974087b99ab5d9ab5dce3d4f73959ff2
jfmennemann <jfmennemann@gmx.de>
2021-01-11 08:52:23 +0100

    modify figure related to coherent state

--- src/style_sheet/mystyle.py
+++ src/style_sheet/mystyle.py
@@ -14,12 +14,46 @@ color_u_ref = mycolors.peter_river
 color_u     = mycolors.wet_asphalt
 
 
+
+color_rel_error_cn_2 = mycolors.wet_asphalt
+color_rel_error_cn_4 = mycolors.wet_asphalt
+color_rel_error_cn_6 = mycolors.wet_asphalt
+color_rel_error_cn_8 = mycolors.wet_asphalt
+
+color_rel_error_rk4_2 = mycolors.alizarin
+color_rel_error_rk4_4 = mycolors.alizarin
+color_rel_error_rk4_6 = mycolors.alizarin
+color_rel_error_rk4_8 = mycolors.alizarin
+
+
+
 linewidth_u_ref = 1.50
 linewidth_u     = 1.25
+
+linewidth_rel_error_cn_2 = 1.25
+linewidth_rel_error_cn_4 = 1.25
+linewidth_rel_error_cn_6 = 1.25
+linewidth_rel_error_cn_8 = 1.25
+
+linewidth_rel_error_rk4_2 = 1.25
+linewidth_rel_error_rk4_4 = 1.25
+linewidth_rel_error_rk4_6 = 1.25
+linewidth_rel_error_rk4_8 = 1.25
+
+
 
 linestyle_u_ref = '-'
 linestyle_u     = '-'
 
+linestyle_rel_error_cn_2 = ':'
+linestyle_rel_error_cn_4 = '-.'
+linestyle_rel_error_cn_6 = '--'
+linestyle_rel_error_cn_8 = '-'
+
+linestyle_rel_error_rk4_2 = ':'
+linestyle_rel_error_rk4_4 = '-.'
+linestyle_rel_error_rk4_6 = '--'
+linestyle_rel_error_rk4_8 = '-'
 
 
 
@@ -68,7 +102,7 @@ rc_parameters = {
     "legend.shadow": True,
     "legend.handlelength": 3.0,
     # 
-    "pgf.preamble": ("\\usepackage{amsmath}\n\\usepackage{amsfonts}\n\\usepackage{amssymb}\n\\usepackage{bbm}")
+    "pgf.preamble": ("\\usepackage{amsmath}\n\\usepackage{amsfonts}\n\\usepackage{amssymb}\n\\usepackage{bbm}\n\\usepackage{bm}")
     }
 
 
